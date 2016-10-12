9d7a52a5a480721620c3050f3daeb7a99d5e7283
markf94 <markfingerhuth@protonmail.com>
2016-10-12 23:59:32 +0200

    new menu works smooth in pure drawer folder

--- pure-drawer-master/dist/js/scripts.js
+++ pure-drawer-master/dist/js/scripts.js
@@ -73,19 +73,6 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
 	this.classList.toggle("active");
 });
 
-/* When user clicks the Icon */
-	$('.nav-toggle').click(function() {
-		$(this).toggleClass('active');
-		$('.header-nav').toggleClass('open');
-		event.preventDefault();
-	});
-	/* When user clicks a link */
-	$('.header-nav li a').click(function() {
-		$('.nav-toggle').toggleClass('active');
-		$('.header-nav').toggleClass('open');
-
-	});
-
 /***************** Overlays ******************/
 
 $(document).ready(function(){
